8c977ab9458f62aa62b8a4fa49888b343a22d1b5
toannc <toan.nguyen-chi@lazada.com>
2017-06-04 19:13:01 +0700

    First Initial

---
+++ main.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://peaceful_swanson/dev';
+var str = "";
+
+app.route('/').get(function(req, res)
+
+    {
+        MongoClient.connect(url, function(err, db) {
+            var cursor = db.collection('users').find();
+            //noinspection JSDeprecatedSymbols
+            cursor.each(function(err, item) {
+
+                if (item != null) {
+                    str = str + "    username  " + item.username + "</br>";
+                }
+            });
+            res.send(str);
+            db.close();
+        });
+    });
+
+var server = app.listen(3010, function() {});
